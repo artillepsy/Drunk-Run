d5886e361004700484f63fd194c5adb94a51c60c
Onexerox <86905093+Onexerox@users.noreply.github.com>
2022-06-17 15:29:41 +0300

    Added walking cycle offset to human animator

---
+++ Assets/Scripts/Human/HumanAsyncWalkAnimationSetter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Human
+{
+    public class HumanAsyncWalkAnimationSetter : MonoBehaviour
+    {
+        private static readonly int CycleOffset = Animator.StringToHash("CycleOffset");
+        
+        private void Awake()
+        {
+            var animator = GetComponentInChildren<Animator>();
+            var randomOffset = Random.Range(0, animator.GetCurrentAnimatorStateInfo(0).length);
+            animator.SetFloat(CycleOffset, randomOffset);
+        }
+    }
+}
