59ccc4b710165824e7af61ca90baccbc898c6f57
Onexerox <86905093+Onexerox@users.noreply.github.com>
2022-06-23 15:32:19 +0300

    Started working on appMetrica events

---
+++ Assets/Scripts/Levels/LevelData.cs
@@ -0,0 +1,44 @@
+﻿using HumanAttraction;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Levels
+{
+    public class LevelData : MonoBehaviour
+    {
+        private static bool s_isInitialized;
+
+        private static int s_games_count = 0;
+        private static int s_level_number = 0;
+        private static string s_level_name = "";        
+        
+        private void Awake()
+        {
+            if (!s_isInitialized)
+            {
+                s_isInitialized = true;
+                DontDestroyOnLoad(gameObject);
+            }
+            else Destroy(gameObject);
+            
+            AttractorForwardMover.OnReachedEnd.AddListener(SendLevelEndEvent);
+        }
+
+        private void Start() => SendLevelStartEvent();
+
+        private void SendLevelStartEvent()
+        {
+            if (!string.Equals(s_level_name, SceneManager.GetActiveScene().name))
+            {
+                s_level_number++;
+            }
+            
+            AppMetrica.Instance.ReportEvent("level_start");
+        }
+
+        private void SendLevelEndEvent()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Levels/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using HumanAttraction;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Levels
+{
+    public class LevelManager : MonoBehaviour
+    {
+        [SerializeField] private string nextLevelName = "Level_1";
+        [SerializeField] private float reloadDelay = 2f;
+        private void Start()
+        {
+            AttractorForwardMover.OnReachedEnd.AddListener(() =>
+            {
+                Invoke(nameof(LoadNextLevel), reloadDelay);
+            });
+        }
+
+        private void LoadNextLevel() => SceneManager.LoadSceneAsync(nextLevelName);
+
+        
+    }
+}
